93ceea5dd9441c6a32bdb8f7412bc396b6181a50
Jared Hensley <jahensle85@me.com>
2017-04-30 16:02:13 -0500

    video player component added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
+// create a new component
+// this component should produce some html
+
+// take this components generated html and put it on the page (aka in the DOM)
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBjLci1Pp-75JSezytc4nqg-NU-lhw4m_k';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, search: 'surfboards'}, videos => {
+      this.setState({ videos })
+    })
+  } 
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
